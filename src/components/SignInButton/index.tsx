797e528d12855043391d42d6962510816edcb034
luccasecco <luccasecco@hotmail.com>
2022-07-06 14:25:34 -0300

    projeto integrado com stripe e faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,33 +1,35 @@
-import {FaGithub} from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { FaGithub } from "react-icons/fa"
+import { FiX } from "react-icons/fi"
+import { signIn, signOut, useSession } from 'next-auth/react'
+import styles from "./styles.module.scss"
 
-import styles from './styles.module.scss'
+function SignInButton() {
+    const { data: Session } = useSession()
 
-export function  SignInButton() {
-  const isUserLoggedIn = true;
-
-
-  return isUserLoggedIn ? (
-    <button 
-      type='button'
-      className={styles.signInButton}
-    >
-      <FaGithub color="#04b361" />
-      Lucca Secco
-      <FiX color="#737380" className={styles.closeIcon} />
-    </button>
-
-  ) : (
-
-    <button 
-      type='button'
-      className={styles.signInButton}
-    >
-      <FaGithub color="#eba417" />
-      Sign in with Github
-    </button>
-
-  )
-  
-  
+    return Session ? (
+        <button
+            className={styles.signInButton}
+            type="button"
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#04d361"/>
+            {Session.user.name}
+            <FiX color="#737380" className={styles.closedIcon}/>
+        </button>
+    ) :
+     (
+        <button
+            className={styles.signInButton}
+            type="button"
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#eba417"/>
+            Sign in with Github
+        </button>
+    );
 }
+
+export default SignInButton;
+
+
+
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -5,7 +5,7 @@ export const stripe = new Stripe(
   {
     apiVersion: '2020-08-27',
     appInfo: {
-      name: 'ig.news',
+      name: 'Ignews',
       version: '0.1.0'
     },
   }
